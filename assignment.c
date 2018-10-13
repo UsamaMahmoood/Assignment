2c8ef3a908c1267c52a33f0df4e1fc87329c08f5
UsamaMahmoood <44072601+UsamaMahmoood@users.noreply.github.com>
2018-10-13 20:24:07 +0500

    Update assignment.c

--- assignment.c
+++ assignment.c
@@ -6,96 +6,110 @@
 
 int main ()
 {
-    int ar [1000];
-    int sum = 0;
-    int s1=0,s2=0,s3=0,s4=0,s5=0,s6=0,s7=0,s8=0,s9=0,s10=0;
-    int i = 0;
+    int ar [1000], sum = 0, i;
+    int hsum[10][2];
+    for(i = 0; i < 10; i++)
+        pipe(hsum[i]);
     for(i = 0; i < 1000; i++)
         ar[i] = i + 1;
     int cpid = fork();
     if(cpid == 0){
-         printf("Total sum of 1000 numbers is: %d\n",sum);
+        for(int i=0;i<100;i++)
+                sum = sum + ar[i];
+            write(hsum[0][1], &sum , sizeof(sum));
+            close(hsum[0][1]);
+            exit(0);
     }
     else{
         int cpid1 = fork();
         if(cpid1 == 0){
-            for(int i = 0; i < 100; i++)
-                s1 = s1 + ar[i];
-                printf("Total sum of 100 numbers is: %d\n",s1);
-                sum = sum + s1;
-            }
-      else{
-        int cpid2 = fork();
-        if(cpid2 == 0){
-            for(int i = 100; i < 200; i++)
-                s2 = s2 + ar[i];
-                    printf("Total sum of 100 numbers is: %d\n",s2);
-                    sum = sum + s2;
+            for(int i=100;i<200;i++)
+                sum = sum + ar[i];
+            write(hsum[1][1], &sum , sizeof(sum));
+            close(hsum[1][1]);
+            exit(0);
         }
         else{
-            int cpid3 = fork();
-            if(cpid3 == 0){
-                for(int i = 200; i < 300; i++)
-                    s3 = s3 + ar[i];
-                    printf("Total sum of 100 numbers is: %d\n",s3);
-                    sum = sum + s3;
+            int cpid2 = fork();
+            if(cpid2 == 0){
+                for(int i=200;i<300;i++)
+                    sum = sum + ar[i];
+                write(hsum[2][1], &sum , sizeof(sum));
+                close(hsum[2][1]);
+                exit(0);
             }
             else{
-                int cpid4 = fork();
-                if(cpid4 == 0){
-                    for(int i = 300; i < 400; i++)
-                        s4 = s4 + ar[i];
-                        printf("Total sum of 100 numbers is: %d\n",s4);
-                        sum = sum + s4;
-                }
-                else{
-                    int cpid5 = fork();
-                    if(cpid5 == 0){
-                        for(int i = 400; i < 500; i++)
-                            s5 = s5 + ar[i];
-                            printf("Total sum of 100 numbers is: %d\n",s5);
-                            sum = sum + s5;
+                int cpid3 = fork();
+                if(cpid3 == 0){
+                    for(int i=300;i<400;i++)
+                        sum = sum + ar[i];
+                    write(hsum[3][1], &sum , sizeof(sum));
+                    close(hsum[3][1]);
+                    exit(0);
                     }
+                else{
+                    int cpid4 = fork();
+                    if(cpid4 == 0){
+                        for(int i=400;i<500;i++)
+                            sum = sum + ar[i];
+                        write(hsum[4][1], &sum , sizeof(sum));
+                        close(hsum[4][1]);
+                        exit(0);
+                        }
                     else{
-                        int cpid6 = fork();
-                        if(cpid6 == 0){
-                            for(int i = 500; i < 600; i++)
-                                s6 = s6 + ar[i];
-                                printf("Total sum of 100 numbers is: %d\n",s6);
-                                sum = sum + s6;
+                        int cpid5 = fork();
+                        if(cpid5 == 0){
+                            for(int i=500;i<600;i++)
+                                sum = sum + ar[i];
+                            write(hsum[5][1], &sum , sizeof(sum));
+                            close(hsum[5][1]);
+                            exit(0);
                         }
                         else{
-                            int cpid7 = fork();
-                            if(cpid7 == 0){
-                                for(int i = 600; i < 700; i++)
-                                    s7 = s7 + ar[i];
-                                    printf("Total sum of 100 numbers is: %d\n",s7);
-                                    sum = sum + s7;
+                            int cpid6 = fork();
+                            if(cpid6 == 0){
+                                for(int i=600;i<700;i++)
+                                        sum = sum + ar[i];
+                                write(hsum[6][1], &sum , sizeof(sum));
+                                close(hsum[6][1]);
+                                exit(0);   
                             }
                             else{
-                                int cpid8 = fork();
-                                if(cpid8 == 0){
-                                    for(int i = 700; i < 800; i++)
-                                        s8 = s8 + ar[i];
-                                        printf("Total sum of 100 numbers is: %d\n",s8);
-                                        sum = sum + s8;
+                                int cpid7 = fork();
+                                if(cpid7 == 0){
+                                    for(int i=700;i<800;i++)
+                                        sum = sum + ar[i];
+                                    write(hsum[7][1], &sum , sizeof(sum));
+                                    close(hsum[7][1]);
+                                    exit(0);
                                 }
                                 else{
-                                    int cpid9 = fork();
-                                    if(cpid9 == 0){
-                                        for(int i = 800; i < 900; i++)
-                                            s9 = s9 + ar[i];
-                                            printf("Total sum of 100 numbers is: %d\n",s9);
-                                            sum = sum + s9;
+                                    int cpid8 = fork();
+                                    if(cpid8 == 0){
+                                       for(int i=800;i<900;i++)
+                                            sum = sum + ar[i];
+                                        write(hsum[8][1], &sum , sizeof(sum));
+                                        close(hsum[8][1]);
+                                        exit(0);
                                     }
                                     else{
-                                        int cpid10 = fork();
-                                        if(cpid10 == 0){
-                                            for(int i = 900; i < 1000; i++)
-                                                s10 = s10 + ar[i];
-                                                printf("Total sum of 100 numbers is: %d\n",s10);
-                                                sum = sum + s10;
+                                        int cpid9 = fork();
+                                        if(cpid9 == 0){
+                                            for(int i=900;i<1000;i++)
+                                                sum = sum + ar[i];
+                                            write(hsum[9][1], &sum , sizeof(sum));
+                                            close(hsum[9][1]);
+                                            exit(0);
                                         }
-    }}}}}}}}}}
+  }}}}}}}}}
+    for(int i = 0; i < 10; i++) 
+        wait(NULL);
+    int hsum2 = 0, totalSum = 0;
+    for(int i = 0; i < 10; i++){
+            read(hsum[i][0], &hsum2, sizeof(int));
+            close(hsum[i][0]);
+            totalSum = totalSum + hsum2;
+    }
+    printf("TOTAL SUM IS: %d",totalSum);
   return 0;
 }
